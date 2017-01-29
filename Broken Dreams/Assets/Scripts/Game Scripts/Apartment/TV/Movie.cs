a714b5f495e326e46e8df9de4149fee255f517ac
Boris Pettersson <br.cupofteagames@gmail.com>
2017-01-28 20:18:42 -0400

    Added TV Animation & Organized Folders
    - Created new folders to place assets for the apartment.
    - Created a new camera that will relay to the mesh in front of the TV to
    simulate functionality and animation.

---
+++ Broken Dreams/Assets/Scripts/Game Scripts/Apartment/TV/Movie.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movie : MonoBehaviour
+{
+
+    private MovieTexture mtex;
+
+    // Use this for initialization
+    void Start()
+    {
+        mtex = gameObject.GetComponent<Renderer>().material.mainTexture as MovieTexture;
+        mtex.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!mtex.isPlaying)
+        {
+            print("movie playing status: " + mtex.isPlaying + ".");
+            mtex.Play();
+        }
+    }
+}
