581d7f41298f0e36e3e91f6dc8b3c31541377a03
Boris Pettersson <br.cupofteagames@gmail.com>
2017-02-27 00:32:09 -0400

    Updated Scene

--- Broken Dreams/Assets/Scripts/Game Scripts/Apartment/PC/Computer.cs
+++ Broken Dreams/Assets/Scripts/Game Scripts/Apartment/PC/Computer.cs
@@ -21,6 +21,10 @@ public class Computer : MonoBehaviour {
         {
             LockMouse.lockMouse = true;
         }
+        else
+        {
+            LockMouse.lockMouse = false;
+        }
     }
 
     void FixedUpdate()
