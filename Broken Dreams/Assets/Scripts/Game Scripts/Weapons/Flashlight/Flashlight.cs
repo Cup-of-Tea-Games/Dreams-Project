019f6b5b628a4dc8df7fa9df51a24fb7537cbf7a
Boris Pettersson <br.cupofteagames@gmail.com>
2017-07-28 13:03:53 -0400

    Applied Anims to weapon mechanics

---
+++ Broken Dreams/Assets/Scripts/Game Scripts/Weapons/Flashlight/Flashlight.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flashlight : MonoBehaviour {
+
+    //Health
+    public static float health;
+    public Light spotlight;
+    bool toggleSwitch = false;
+
+    //Animation
+    public Animator animator;
+
+    //Functionality
+
+  //  public Camera player;
+  //  public float damage = 5;
+  //  public float range = 5;
+  //  public float force = 5;
+    bool canSwitch = true;
+
+    //VIsuals
+
+ //   public GameObject flashEffect;
+    // public ParticleSystem muzzleFlash;
+
+    void Update()
+    {
+        //Animations
+
+        //Attack
+        if (Input.GetMouseButtonDown(0) && !WeaponWheel.isShowing)
+        {
+            if (canSwitch)
+                StartCoroutine(Switch(1f));
+        }
+
+    }
+
+    IEnumerator Switch(float x)
+    {
+        canSwitch = false;
+        animator.Play("Switch");
+        yield return new WaitForSeconds(x/2);
+        Toggle();
+        yield return new WaitForSeconds(x);
+        canSwitch = true;
+        StopCoroutine(Switch(x));
+    }
+
+    //Functionality
+
+    void Toggle()
+    {
+        toggleSwitch = !toggleSwitch;
+        spotlight.enabled = toggleSwitch;
+    }
+
+
+    /*
+    void Fire()
+    {
+        //   muzzleFlash.Play();
+
+        RaycastHit hit;
+        if (Physics.Raycast(player.transform.position, player.transform.forward, out hit, range, 1 << LayerMask.NameToLayer("Default")))
+        {
+            Debug.Log(hit.transform.name);
+
+            if (hit.transform.GetComponent<DestroyableObject>() != null)
+            {
+                hit.transform.GetComponent<DestroyableObject>().takeDamage(damage);
+            }
+
+            else if (hit.transform.GetComponent<DamagePoint>() != null)
+            {
+                hit.transform.GetComponent<DamagePoint>().takeDamage(damage);
+            }
+
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * 1000 * force);
+            }
+
+            GameObject effectParticle = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal)) as GameObject;
+            Destroy(effectParticle, 2f);
+        }
+    }
+
+    */
+}
