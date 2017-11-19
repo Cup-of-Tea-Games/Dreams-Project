9e6d5eea75bd4a2519c43ec30a0678f5cbf0b19a
Boris Pettersson <br.cupofteagames@gmail.com>
2017-11-19 13:16:53 -0400

    Added TGemporary Animations to arms

---
+++ Broken Dreams/Assets/Scripts/Game Scripts/FPS/Animated Controls/ArmsAnimator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmsAnimator : MonoBehaviour {
+
+    public Animator m_Animator;
+    public Animator bodyAnimator;
+
+    void Update()
+    {
+        m_Animator.SetFloat("Forward", bodyAnimator.GetFloat("Forward"), 0.1f, Time.deltaTime);
+        m_Animator.SetFloat("Side", bodyAnimator.GetFloat("Side"), 0.1f, Time.deltaTime);
+        m_Animator.SetBool("Crouch", bodyAnimator.GetBool("Crouch"));
+        m_Animator.SetBool("OnGround", bodyAnimator.GetBool("OnGround"));
+    }
+}
