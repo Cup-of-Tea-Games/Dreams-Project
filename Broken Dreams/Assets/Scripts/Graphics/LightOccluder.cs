df195315188d0faf6e6418db4a922dae293bce53
Boris Pettersson <br.cupofteagames@gmail.com>
2017-12-05 23:38:25 -0400

    Added temporary distance light culler scripts

---
+++ Broken Dreams/Assets/Scripts/Graphics/LightOccluder.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class LightOccluder : MonoBehaviour {
+
+    //   public GameObject
+
+    GameObject player;
+    public float distanceOcclude = 3;
+
+    void Awake()
+    {
+        player = GameObject.Find("Player");
+    }
+
+	void Update () {
+        //   Debug.Log("CURRENT STATUS : " + gameObject.GetComponent<Renderer>().isVisible);
+
+        //    gameObject.GetComponent<Renderer>().enabled = gameObject.GetComponent<Renderer>().isVisible;
+
+        float distance = Vector3.Distance(gameObject.transform.position, player.transform.position);
+
+        if (distance > distanceOcclude)
+        {
+            gameObject.GetComponent<Light>().enabled = false;
+        }
+        else
+            gameObject.GetComponent<Light>().enabled = true;
+
+    }
+}
