8d795cccf9a5689f4a35b69dab0cfa9e8be28a09
Boris Pettersson <br.cupofteagames@gmail.com>
2017-12-05 23:48:14 -0400

    Mesh occluder script

---
+++ Broken Dreams/Assets/Scripts/Graphics/MeshOccluder.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshOccluder : MonoBehaviour {
+
+    //   public GameObject
+
+    GameObject player;
+    public float distanceOcclude = 3;
+
+    void Awake()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    void Update()
+    {
+        //   Debug.Log("CURRENT STATUS : " + gameObject.GetComponent<Renderer>().isVisible);
+
+        //    gameObject.GetComponent<Renderer>().enabled = gameObject.GetComponent<Renderer>().isVisible;
+
+        float distance = Vector3.Distance(gameObject.transform.position, player.transform.position);
+
+        if (distance > distanceOcclude)
+        {
+            gameObject.GetComponent<Renderer>().enabled = false;
+        }
+        else
+            gameObject.GetComponent<Renderer>().enabled = true;
+
+    }
+}
