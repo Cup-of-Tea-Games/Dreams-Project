17367dcdd300bc630b9a693d6ae91864d9710c6b
Boris Pettersson <br.cupofteagames@gmail.com>
2017-12-08 02:01:46 -0400

    Pusher

--- Broken Dreams/Assets/Scripts/Graphics/ROS/MOS.cs
+++ Broken Dreams/Assets/Scripts/Graphics/ROS/MOS.cs
@@ -7,36 +7,25 @@ public class MOS : MonoBehaviour
 
     GameObject player;
     public Room[] rooms;
+    public bool resMode;
+    int qualitylevel;
+    public bool parentMode = false;
+    Vector2 originalScale;
 
     void Awake()
     {
         player = GameObject.Find("Player");
+        qualitylevel = QualitySettings.GetQualityLevel();
+        originalScale = gameObject.GetComponent<Material>().mainTextureScale;
     }
 
     void Update()
     {
-
-        if (!isInArea())
-        {
-            foreach (Renderer gb in GetComponentsInChildren<Renderer>())
-                if (GetComponentsInChildren<Renderer>() != null)
-                {
-                    gb.GetComponent<Renderer>().enabled = false;
-                }
-        }
+        if (parentMode)
+            parentfunc();
         else
-        {
-            foreach (Renderer gb in GetComponentsInChildren<Renderer>())
-                if (GetComponentsInChildren<Renderer>() != null)
-                {
-                    gb.GetComponent<Renderer>().enabled = true;
-                }
-        }
-
-
-
+            solofunc();
     }
-
 
     bool isInArea()
     {
@@ -51,6 +40,58 @@ public class MOS : MonoBehaviour
 
         }
         return temp;
+    }
+
+    void parentfunc()
+    {
+        if (!isInArea())
+        {
+            for (int i = 0; (i < gameObject.GetComponentsInChildren<Transform>().Length) && (gameObject.GetComponentsInChildren<Light>() != null); i++)
+            {
+             //   if (!resMode)
+                 //   gameObject.GetComponentsInChildren<Material>()[i].
+             //   else
+                {
+                    gameObject.GetComponentsInChildren<Material>()[i].mainTextureScale /= 2;
+                }
+            }
+        }
+        else
+        {
+            for (int i = 0; (i < gameObject.GetComponentsInChildren<Transform>().Length) && (gameObject.GetComponentsInChildren<Light>() != null); i++)
+            {
+             //   if (!resMode)
+             //       gameObject.GetComponentsInChildren<Light>()[i].enabled = true;
+             //   else
+                {
+                    gameObject.GetComponentsInChildren<Material>()[i].mainTextureScale = originalScale;
+                }
+            }
+        }
+    }
+
+    void solofunc()
+    {
+        if (!isInArea())
+        {
+            if (!resMode)
+                gameObject.GetComponent<Light>().enabled = false;
+            else
+            {
+                gameObject.GetComponent<Material>().color = Color.black;
+                //    gameObject.GetComponent<Light>().shadows = LightShadows.None;
+            }
+        }
+        else
+        {
+            if (!resMode)
+                gameObject.GetComponent<Light>().enabled = true;
+            else
+            {
+                gameObject.GetComponent<Material>().color = Color.white;
+                //   gameObject.GetComponent<Light>().shadows = LightShadows.Soft;
+            }
+        }
     }
 
 }
