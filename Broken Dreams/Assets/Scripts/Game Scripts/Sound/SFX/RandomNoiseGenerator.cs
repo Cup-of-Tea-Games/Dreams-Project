06bf2213f1388360b1dc97dba0795f3177b3be09
Boris Pettersson <br.cupofteagames@gmail.com>
2018-01-19 18:52:25 -0400

    Wandrer sfx scripts

---
+++ Broken Dreams/Assets/Scripts/Game Scripts/Sound/SFX/RandomNoiseGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomNoiseGenerator : MonoBehaviour {
+
+    public AudioClip[] clips;
+    private AudioSource source;
+
+    void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+
+        if (!source.isPlaying)
+        {
+            StartCoroutine(switchTrack());
+        }
+
+    }
+
+    IEnumerator switchTrack()
+    {
+        int x = Random.RandomRange(0, clips.Length);
+
+        yield return new WaitForSeconds(2);
+        source.PlayOneShot(clips[x]);
+        StopCoroutine(switchTrack());
+    }
+
+    bool isPlaying()
+    {
+        bool temp = false;
+
+        for (int i = 0; i < clips.Length; i++)
+        {
+        
+        }
+
+        return temp;
+    }
+
+}
