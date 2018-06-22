550a0af556176bb20e2304c2e38e61ecfa93d0ce
Boris Pettersson <br.cupofteagames@gmail.com>
2018-06-22 13:54:36 -0400

    Cosmetics
    -Fixed death menu template style
    -Added decorative skeletons
    -Added human hearts to furnace room
    -Imported PBR graph assets

--- Broken Dreams/Assets/Scripts/Game Scripts/Triggers/ProxyTrigger.cs
+++ Broken Dreams/Assets/Scripts/Game Scripts/Triggers/ProxyTrigger.cs
@@ -5,24 +5,63 @@ public class ProxyTrigger : MonoBehaviour {
 
     public GameObject gameThing;
     public string tagName;
+    public bool oneTimeUse = true;
+    public bool invertFunction = false;
+    public bool forwardsBackwards = false;
     public bool destroyInsteadOfSpawn = false;
     public bool triggerButton = false;
     public Button button;
 
     void OnTriggerEnter(Collider col)
     {
-        if(col.tag == tagName && !triggerButton)
+        if (!invertFunction)
         {
-            if (!destroyInsteadOfSpawn)
+            if (col.tag == tagName && !triggerButton)
             {
-                gameThing.SetActive(true);
+                if (!destroyInsteadOfSpawn)
+                {
+                    gameThing.SetActive(true);
+                }
+                else
+                {
+                    // gameThing.SetActive(false);
+                    Destroy(gameThing);
+                }
+                if(oneTimeUse)
+                Destroy(gameObject);
             }
-            else
+            else if (forwardsBackwards)
             {
-                // gameThing.SetActive(false);
-                Destroy(gameThing);
+                if (!destroyInsteadOfSpawn)
+                {
+                    gameThing.SetActive(false);
+                }
             }
-            Destroy(gameObject);
+        }
+
+        else if (invertFunction)
+        {
+            if (col.tag == tagName && !triggerButton)
+            {
+                if (!destroyInsteadOfSpawn)
+                {
+                    gameThing.SetActive(false);
+                }
+                else
+                {
+                    // gameThing.SetActive(false);
+                    Destroy(gameThing);
+                }
+                if (oneTimeUse)
+                    Destroy(gameObject);
+            }
+            else if (forwardsBackwards)
+            {
+                if (!destroyInsteadOfSpawn)
+                {
+                    gameThing.SetActive(true);
+                }
+            }
         }
 
         if (col.tag == tagName && triggerButton)
